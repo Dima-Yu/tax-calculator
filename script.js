d7caae4c9318e97be3441ff27238fca51c17e774
Dima-Yu <mindwork.one@gmail.com>
2018-12-19 19:16:44 +0200

    new file for class calculation

--- script.js
+++ script.js
@@ -51,38 +51,6 @@ function taxCalc() {
     params.exciseRate = selectedOption == 'gas' ? 50 : 75;
   }
 
-  class Calculation {
-    feeCalc() {
-      totals.fee = params.carValue * params.rateOfFee;
-    }
-    exiceCalc() {
-      totals.exice = params.exciseRate * (params.engineCapacity / 1000) * params.carAge;
-    }
-    incomeTaxCalc() {
-      totals.incomeTaxBasis = + params.carValue + totals.fee + totals.exice;
-      totals.incomeTax = totals.incomeTaxBasis * params.incomeTax;
-    }
-    totalValueCalc() {
-      totals.value = totals.fee + totals.exice + totals.incomeTax;
-    }
-
-    call(){
-      this.feeCalc();
-      this.exiceCalc();
-      this.incomeTaxCalc();
-      this.totalValueCalc();
-
-      resultTableCell.carValue.innerHTML = params.carValue;
-      resultTableCell.engineCapacity.innerHTML = params.engineCapacity + ' cm3';
-      resultTableCell.exciseRate.innerHTML = params.exciseRate + '€';
-      resultTableCell.totalRate.innerHTML = totals.fee;
-      resultTableCell.totalExcise.innerHTML = totals.exice;
-      resultTableCell.incomeTaxBasis.innerHTML = totals.incomeTaxBasis;
-      resultTableCell.totalIncomeTax.innerHTML = totals.incomeTax;
-      resultTableCell.totalValue.innerHTML = totals.value;
-    }
-  }
-
   formField.calcBtn.onclick = function() {
     let result = new Calculation().call();
   }
